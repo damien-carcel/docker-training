9aa3144a1ade05426b0cb5a5b843e5c41bd4968a
Damien Carcel <damien.carcel@akeneo.com>
2023-07-04 17:56:06 +0200

    Ensure presentation is 16/9

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 import 'reveal.js/dist/reveal.css';
-import 'reveal.js/dist/theme/black.css';
+import 'reveal.js/dist/theme/league.css';
 /* Code syntax highlighting */
-import 'reveal.js/plugin/highlight/zenburn.css';
+import 'reveal.js/plugin/highlight/monokai.css';
+
+import './style.css';
 
 import Reveal from 'reveal.js';
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight';
+import RevealNotes from 'reveal.js/plugin/notes/notes';
 
-const deck = new Reveal();
-deck.initialize();
+const deck = new Reveal({
+  plugins: [RevealHighlight, RevealNotes],
+});
+deck.initialize({
+  width: 1280,
+  height: 720,
+});
